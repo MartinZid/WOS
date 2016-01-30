9c95fa56864b1898aee0ca9f1c2868cbab46cf48
Martin Žid <martin.zid@gmail.com>
2016-01-30 13:11:43 +0100

    init

---
+++ WOS/bld/Ripple/iOS/Debug/controllers/controllers.js
@@ -0,0 +1,17 @@
+angular.module('wos.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope) {
+
+})
+
+.controller('ChatDetailCtrl', function($scope) {
+
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
